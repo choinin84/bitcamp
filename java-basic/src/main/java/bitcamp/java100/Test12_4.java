11e0f26e0c7fac6e73ee3b8e5bbe2cbfb9571e8c
choinin84 <choinin84@naver.com>
2017-09-27 13:43:31 +0900

    '..'

---
+++ java-basic/src/main/java/bitcamp/java100/Test12_4.java
@@ -0,0 +1,27 @@
+package bitcamp.java100;
+
+public class Test12_4 {
+
+public static void main (String[] args) {
+
+int i;
+
+i = 100;
+i = 200;
+
+final int j;
+
+j = 200;
+j = 300;
+
+
+
+
+
+    
+
+
+
+
+    }
+}
