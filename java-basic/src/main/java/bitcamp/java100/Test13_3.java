fce9a0b9020ce9150580a34d9fbbb3023c4ced5f
choinin84 <choinin84@naver.com>
2017-09-28 17:55:55 +0900

    '..'

---
+++ java-basic/src/main/java/bitcamp/java100/Test13_3.java
@@ -0,0 +1,27 @@
+package bitcamp.java100;
+
+public class Test13_3 {
+
+public static void main (String[] args) {
+
+int[] arr1;
+
+arr1 = new int[3];
+
+
+System.out.println(arr1.length);
+
+arr1[0] = 100;
+arr1[1] = 90;
+arr1[2] = 80;
+
+
+
+
+
+
+
+
+
+}
+}
---
+++ java-basic/src/main/java/bitcamp/java100/Test13_4.java
@@ -0,0 +1,14 @@
+package bitcamp.java100;
+
+public class Test13_4 {
+
+public static void main (String[] args) {
+
+int[] arr1 = new int[3];
+
+int[] arr2 = new int[] {100, 90, 80};
+
+
+
+ }
+}
